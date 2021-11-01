2e6d97cd564750fbf87e1d15d5d00303ff4b8819
= <=>
2021-11-01 18:01:13 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'components/barchart';
 import DataTable from 'components/datatable';
+import DonutChart from 'components/donutchart';
 import Fooder from 'components/fooder';
 import NavBar from 'components/navbar';
 import React from 'react';
@@ -8,7 +10,21 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1>Olá mundo</h1>
+        <h1 className="text-primary px-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
         <DataTable />
       </div>  
       <Fooder />
